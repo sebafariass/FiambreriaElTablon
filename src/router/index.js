38a20ba128fb21dc5d492d6822288221c35f6c89
Sebastian <sfarias.arellano@gmail.com>
2021-03-07 12:21:50 -0400

    iniciocommit

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  },
+  {
+    path: "/contacto",
+    name: "Contacto",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Contacto.vue")
+  },
+  {
+    path: "/productos",
+    name: "Productos",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import( "../views/Productos.vue")
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+   <Navbar />
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <Vistas msg="Welcome to Your Vue.js App" />
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Vistas from "@/components/Vistas.vue";
+import Navbar from "@/components/Navbar.vue";
+
+
+
+export default {
+  name: "Home",
+  components: {
+    Vistas,
+    Navbar
+    
+  }
+};
+</script>
